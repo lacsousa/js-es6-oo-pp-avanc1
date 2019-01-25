1fdd2df208f79cb9ab2e6a9e84b18d8a90cd0075
Luciano Cordeiro <cyft@petrobras.com.br>
2019-01-25 08:50:50 -0300

    Término Cap.01

--- aluraframe/client/js/app/models/Negociacao.js
+++ aluraframe/client/js/app/models/Negociacao.js
@@ -1,12 +1,31 @@
 class Negociacao{
 
     constructor(data, quantidade, valor){
-        this.data = data;
-        this.quantidade = quantidade;
-        this.valor = valor;
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
+        Object.freeze(this); // Protegendo o Objeto, 
+                            //fazendo com que ele fique imutável
     }
 
-    obtemVolume(){
-        return this.quantidade * this.valor;
+    get volume(){
+        return this._quantidade * this._valor;
+    }
+
+    // get data(){
+    //     return this._data;
+    // }
+
+    //Programação Defensiva
+    get data(){
+        return new Date(this._data.getTime());
+    }
+
+    get quantidade(){
+        return this._quantidade;
+    }
+
+    get valor(){
+        return this._valor;
     }
 }
---
+++ aluraframe/exemplos/Conta.js
@@ -0,0 +1,29 @@
+class Conta {
+
+    constructor(titular, conta){
+        this._titular = titular;
+        this._conta = conta;
+        this._saldo = 0.0;
+    }
+
+    deposita(valor){
+        console.log('Valor depositado:', valor);
+        this._saldo += valor;
+    }
+
+    get saldo(){
+        return this._saldo;
+    }
+
+    get titular() {
+        return this._titular;
+    }
+
+    get conta() {
+        return this._conta;
+    }
+    
+    // Podemos criar uma espécie de método, mas que é acessado
+    // como se fosse uma propriedade.
+}
+
