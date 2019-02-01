e31b8282b54f1a2784f31b791c26a96ea8393688
Luciano Cordeiro <cyft@petrobras.com.br>
2019-02-01 13:58:12 -0300

    Trabalhando e melhorando as datas

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -16,17 +16,19 @@ class NegociacaoController {
     adiciona(event){
         event.preventDefault();
         console.log('Passei no Controller de Negociação');
-
-      
-      
+     
         // console.log(this._inputData.value);
         // console.log(this._inputQuantidade.value);
         // console.log(this._inputValor.value);
 
         // Problema com datas
         //Vamos usar o Spread ( ... ) ( oeprador ES6 que separa os parâmetros do construtor)
-        let data = new Date(... this._inputData.value.
-            split('-')
+
+      
+        let data = DateHelper.textoParaData(this._inputData.value);
+            
+        /*
+        split('-')
             // Arrow function 
             // Vc retira o function e se vc só tiver 1 linha no bloco
             // vc pode reduzir mais ainda 
@@ -34,10 +36,13 @@ class NegociacaoController {
         // Tudo isso. porque o mês nesse Date
         // inicia com 0 ( Janeiro )
         console.log(data);
-
+*/
         let negociacao = new Negociacao(data,
             this._inputQuantidade.value, this._inputValor.value);
+
         console.log(negociacao);
+
+        console.log(DateHelper.dataParaTexto(negociacao.data));
     }
 
     /*
---
+++ aluraframe/client/js/app/helpers/DateHelper.js
@@ -0,0 +1,16 @@
+class DateHelper {
+
+    constructor(){
+        throw new Error('A Classe DateHelper não pode ser instanciada!');
+    }
+
+    static dataParaTexto(data) {
+        return data.getDate() +
+            '/' + (data.getMonth() + 1) +
+            '/' + data.getFullYear();
+    }
+
+    static textoParaData(texto) {
+        return new Date(...texto.split('-').map((item, indice) => item - indice % 2));
+    }
+}
