32c327346af58b426b37bc94ad43ca1f6a41f788
Luciano Cordeiro <lacsousa@yahoo.com.br>
2019-04-14 23:02:46 -0300

    Promises

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -1,13 +1,13 @@
 class NegociacaoController {
 
-    constructor(){
+    constructor() {
         let $ = document.querySelector.bind(document);
         // Forma de implementar um micro Framework
         // Como no jQuery
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-      
+
         /* Começar a trabalhar com o Proxy
         this._listaNegociacoes = new ListaNegociacoes(model => {
             console.log(this); // Quando trocamos o function pelo arrow function (AF)=>
@@ -16,28 +16,28 @@ class NegociacaoController {
             this._negociacoesView.update(model);
         });   */
 
-        this._listaNegociacoes = new Bind ( 
+        this._listaNegociacoes = new Bind(
             new ListaNegociacoes(),
-            new NegociacoesView($('#negociacoesView')), 
-            'adiciona', 'esvazia'); 
-            
+            new NegociacoesView($('#negociacoesView')),
+            'adiciona', 'esvazia');
+
         // primeira update
         // Retirado no Cap. 3.3. ----->   this._negociacoesView.update(this._listaNegociacoes);
         // a estratégia de mudar de let ( variáveis) para o this
         // reduz o acesso ao DOM a apenas 1 vez, mesmo que ocorram 
         // 5000 negociações  
 
-        this._mensagem = new Bind ( 
+        this._mensagem = new Bind(
             new Mensagem(),
-            new MensagemView($('#mensagemView')), 
+            new MensagemView($('#mensagemView')),
             'texto');
     }
 
-    adiciona(event){
+    adiciona(event) {
         event.preventDefault();
-      
+
         //let data = DateHelper.textoParaData(this._inputData.value);
-            
+
         /*
         split('-')
             // Arrow function 
@@ -49,16 +49,16 @@ class NegociacaoController {
         console.log(data);
 */
         // let negociacao = new Negociacao(data,
-            // this._inputQuantidade.value, this._inputValor.value);
+        // this._inputQuantidade.value, this._inputValor.value);
 
         this._listaNegociacoes.adiciona(this._criaNegociacao());
         // Cap.04.06 
         // this._listaNegociacoes.negociacoes.length = 0;
         // this._listaNegociacoes.negociacoes.push(this._criaNegociacao);
-        
+
         // Depois de criada a armadilha vamos comentar o update aqui
         // this._negociacoesView.update(this._listaNegociacoes);
-        
+
         this._mensagem.texto = "Negociação adiconada com sucesso!";
         this._limpaFormulario();
         // console.log(this._listaNegociacoes.negociacoes);
@@ -70,39 +70,92 @@ class NegociacaoController {
         }));
     */
 
-   importaNegociacoes(){
+    importaNegociacoes() {
+        // Trabalhando com o Promise
         let service = new NegociacaoService();
-        
+
+        Promise.all([
+            service.obterNegociacoesDaSemana(),
+            service.obterNegociacoesDaSemanaAnterior(),
+            service.obterNegociacoesDaSemanaRetrasada()]
+        ).then(negociacoes => {
+            negociacoes
+                .reduce((arrayAchatado, array) => arrayAchatado.concat(array), [])
+                .forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+            this._mensagem.texto = 'Negociações importadas com sucesso';    
+            console.log(negociacoes);
+        }).catch(erro => this._mensagem.texto = erro);
+
+        /* 2a. REFATORAÇÃO acima
+        service.obterNegociacoesDaSemana()
+            .then(negociacoes => {
+                negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+                this._mensagem.texto = 'Negociações da semana obtidas com sucesso!';
+            })
+            .catch(erro => this._mensagem.texto = erro);
+
+        service.obterNegociacoesDaSemanaAnterior()
+            .then(negociacoes => {
+                negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+                this._mensagem.texto = 'Negociações da semana obtidas com sucesso!';
+            })
+            .catch(erro => this._mensagem.texto = erro);
+
+        service.obterNegociacoesDaSemanaRetrasada()
+            .then(negociacoes => {
+                negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+                this._mensagem.texto = 'Negociações da semana obtidas com sucesso!';
+            })
+            .catch(erro => this._mensagem.texto = erro);
+        */
+
+        /* 
         //Error-First-Callback
-        service.obterNegociacoesDaSemana((err, negociacoes) => {
-            if(err){
-                this._mensagem.texto = err;
+        service.obterNegociacoesDaSemana((erro, negociacoes) => {
+            if(erro){
+                this._mensagem.texto = erro;
                 return;
             }
-
             negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
-            this._mensagem.texto = 'Negociações importadas com sucesso!';
-        });
-   }
 
-   _limpaFormulario() {
+            service.obterNegociacoesDaSemanaAnterior((erro, negociacoes) => {
+                if(erro){
+                    this._mensagem.texto = erro;
+                    return;
+                }
+                negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+
+                service.obterNegociacoesDaSemanaRetrasada((erro, negociacoes) => {
+                    if(erro){
+                        this._mensagem.texto = erro;
+                        return;
+                    }
+                    negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+                    this._mensagem.texto = 'Negociações Semana importadas com sucesso!';
+                });
+            });
+        });
+        */
+    }
+
+    _limpaFormulario() {
         this._inputData.value = '';
         this._inputQuantidade.value = 1;
         this._inputValor.value = 0.0;
 
         this._inputData.focus();
-   }
+    }
 
-   _criaNegociacao(){
+    _criaNegociacao() {
         return new Negociacao(
             DateHelper.textoParaData(this._inputData.value),
-            this._inputQuantidade.value, 
+            this._inputQuantidade.value,
             this._inputValor.value);
-   }
+    }
 
-   apaga(){
-       this._listaNegociacoes.esvazia();
-    //    this._negociacoesView.update(this._listaNegociacoes);
-       this._mensagem.texto = 'Lista de negociações foi removida com sucesso!';
-   }
+    apaga() {
+        this._listaNegociacoes.esvazia();
+        //    this._negociacoesView.update(this._listaNegociacoes);
+        this._mensagem.texto = 'Lista de negociações foi removida com sucesso!';
+    }
 }
--- aluraframe/client/js/app/services/NegociacaoService.js
+++ aluraframe/client/js/app/services/NegociacaoService.js
@@ -1,42 +1,92 @@
-class NegociacaoService{
+class NegociacaoService {
 
-    obterNegociacoesDaSemana(cb){
-        //    alert('Importando negociações');
-        //Não estamos usando jQuery, usaremos JS Puro
-        let xhr = new XMLHttpRequest();
-        xhr.open('GET', 'negociacoes/semana');
+    obterNegociacoesDaSemana() {
 
-        // Configurações
-        xhr.onreadystatechange = () =>{
-            // Precisa testar o status 200 
-            // Porque um erro é uma resposta válida
-            if(xhr.readyState == 4 && xhr.status == 200){
-                console.log('Obtendo as negociações do Servidor.');
-                
-                // JSON.parse - converte Texto num Objeto JS  
-                // Para cada objeto converte para um instancia de negociacao
-                // No novo array ( map ) vc percorre e adiciona 
-                cb(null, JSON.parse(xhr.responseText)
-                    .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)));
-                    
-                // O Service não deve ter contato com a View
-                // this._mensagem.texto = 'Negociações importadas da semana';    
-            }else{
-                // this._mensagem.texto = 'Não foi possível obter as negociações do Servidor.';
-                console.log('Não foi possível obter as negociações do Servidor.');
-                console.log(xhr.responseText);
-                cb('Não foi possível obter as Negociações', null);
-            }
-        };
+        return new Promise((resolve, reject) => {
 
-        xhr.send();
+            //    alert('Importando negociações');
+            //Não estamos usando jQuery, usaremos JS Puro
+            let xhr = new XMLHttpRequest();
+            xhr.open('GET', 'negociacoes/semana');
 
-        /* Estados Possíveis de uma requisição AJAX
-            0: requisição ainda não iniciada
-            1: conexão com o servidor estabelecida
-            2: requisição recebida
-            3: processando requisição
-            4: requisição está concluída e a resposta está pronta
-        */
+            // Configurações
+            xhr.onreadystatechange = () => {
+                // Precisa testar o status 200 
+                // Porque um erro é uma resposta válida
+                if (xhr.readyState == 4) {
+                    if (xhr.status == 200) {
+                        console.log('Obtendo as negociações do Servidor.');
+                        // JSON.parse - converte Texto num Objeto JS  
+                        // Para cada objeto converte para um instancia de negociacao
+                        // No novo array ( map ) vc percorre e adiciona 
+                        resolve(JSON.parse(xhr.responseText)
+                            .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)));
+
+                        // O Service não deve ter contato com a View
+                        // this._mensagem.texto = 'Negociações importadas da semana';    
+                    } else {
+                        // this._mensagem.texto = 'Não foi possível obter as negociações do Servidor.';
+                        console.log('Não foi possível obter as negociações do Servidor.');
+                        console.log(xhr.responseText);
+                        reject('Não foi possível obter as Negociações');
+                    }
+                }
+            };
+
+            xhr.send();
+
+            /* Estados Possíveis de uma requisição AJAX
+                0: requisição ainda não iniciada
+                1: conexão com o servidor estabelecida
+                2: requisição recebida
+                3: processando requisição
+                4: requisição está concluída e a resposta está pronta
+            */
+        });
+    }
+
+    obterNegociacoesDaSemanaAnterior() {
+
+        return new Promise((resolve, reject) => {
+
+            let xhr = new XMLHttpRequest();
+            xhr.open('GET', 'negociacoes/anterior');
+
+            xhr.onreadystatechange = () => {
+                if (xhr.readyState == 4) {
+                    if (xhr.status == 200) {
+                        resolve(JSON.parse(xhr.responseText)
+                            .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)));
+                    } else {
+                        console.log(xhr.responseText);
+                        reject('Não foi possível obter as Negociações da Semana anterior');
+                    }
+                }
+            };
+            xhr.send();
+        });
+    }
+
+    obterNegociacoesDaSemanaRetrasada() {
+
+        return new Promise((resolve, reject) => {
+
+            let xhr = new XMLHttpRequest();
+            xhr.open('GET', 'negociacoes/retrasada');
+
+            xhr.onreadystatechange = () => {
+                if (xhr.readyState == 4) {
+
+                    if (xhr.status == 200) {
+                        resolve(JSON.parse(xhr.responseText)
+                            .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)));
+                    } else {
+                        console.log(xhr.responseText);
+                        reject('Não foi possível obter as Negociações da Semana Retrasada');
+                    }
+                }
+            };
+            xhr.send();
+        });
     }
 }
