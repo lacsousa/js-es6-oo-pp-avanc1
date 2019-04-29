cfd517f7b10506f67d5a2f8857a56cfbd418ea6d
Luciano Cordeiro <lacs@petrobras.com.br>
2019-04-29 15:19:38 -0300

    Criando ConnectionFactory

--- aluraframe/client/js/app/services/ConnectionFactory.js
+++ aluraframe/client/js/app/services/ConnectionFactory.js
@@ -4,27 +4,41 @@ var dbName = 'aluraframe';
 
 class ConnectionFactory {
 
-    constructor(){
+    constructor() {
         throw new Error('Não é possível criar instâncias de ConnectionFactory');
     }
 
     static getConnection() {
 
         return new Promise((resolve, reject) => {
-  
-            let openRequest = window.indexedDB.open(dbName,version);
-  
+
+            let openRequest = window.indexedDB.open(dbName, version);
+
             openRequest.onupgradeneeded = e => {
-
+                ConnectionFactory._createStores(e.target.result);
             };
-  
-            openRequest.onsuccess = e => {   
 
+            openRequest.onsuccess = e => {
+                resolve(e.target.result);
             };
-  
-            openRequest.onerror = e => {  
-                  
+
+            openRequest.onerror = e => {
+                console.log(e.target.error);
+                reject(e.target.error.name);
             };
         });
-  }
+    }
+
+    static _createStores(connection) {
+        //criando novas stores
+        stores.forEach(strore => {
+            if (e.target.result.objectStoreNames.contains(store))
+                e.target.result.deleteObjectStore(store);
+
+            e.target.result.createObjectStore(store, {
+                autoIncrement: true
+            });
+        });
+
+    }
 }
