baee19f378f2da5ebac4900b58be55fd2c0a423c
Luciano Cordeiro <lacs@petrobras.com.br>
2019-04-30 13:59:02 -0300

    Padrão de projeto Module Pattern

--- aluraframe/client/js/app/services/ConnectionFactory.js
+++ aluraframe/client/js/app/services/ConnectionFactory.js
@@ -1,44 +1,54 @@
-var stores = ['negociacoes'];
-var version = 4;
-var dbName = 'aluraframe';
+var ConnectionFactory = (function () {
+// Essa conversão da Classe numa Função Anônima e retornando variáveis 
+// e um Classe é um Padrão de Projeto chamado Module
 
-class ConnectionFactory {
+    var stores = ['negociacoes'];
+    var version = 4;
+    var dbName = 'aluraframe';
 
-    constructor() {
-        throw new Error('Não é possível criar instâncias de ConnectionFactory');
-    }
+    var connection = null;
 
-    static getConnection() {
+    return class ConnectionFactory {
 
-        return new Promise((resolve, reject) => {
+        constructor() {
+            throw new Error('Não é possível criar instâncias de ConnectionFactory');
+        }
 
-            let openRequest = window.indexedDB.open(dbName, version);
+        static getConnection() {
 
-            openRequest.onupgradeneeded = e => {
-                ConnectionFactory._createStores(e.target.result);
-            };
+            return new Promise((resolve, reject) => {
 
-            openRequest.onsuccess = e => {
-                resolve(e.target.result);
-            };
+                let openRequest = window.indexedDB.open(dbName, version);
 
-            openRequest.onerror = e => {
-                console.log(e.target.error);
-                reject(e.target.error.name);
-            };
-        });
-    }
+                openRequest.onupgradeneeded = e => {
+                    ConnectionFactory._createStores(e.target.result);
+                };
 
-    static _createStores(connection) {
-        //criando novas stores
-        stores.forEach(strore => {
-            if (e.target.result.objectStoreNames.contains(store))
-                e.target.result.deleteObjectStore(store);
+                openRequest.onsuccess = e => {
 
-            e.target.result.createObjectStore(store, {
-                autoIncrement: true
+                    //0, "", null, undefined .. tudo isso é Falso
+                    if (!connection) connection = e.target.result;
+                    resolve(connection);
+                };
+
+                openRequest.onerror = e => {
+                    console.log(e.target.error);
+                    reject(e.target.error.name);
+                };
             });
-        });
+        }
 
+        static _createStores(connection) {
+            //criando novas stores
+            stores.forEach(strore => {
+                if (e.target.result.objectStoreNames.contains(store))
+                    e.target.result.deleteObjectStore(store);
+
+                e.target.result.createObjectStore(store, {
+                    autoIncrement: true
+                });
+            });
+
+        }
     }
-}
+})();
