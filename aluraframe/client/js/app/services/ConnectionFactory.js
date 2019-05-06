fb5b975e51be877d5a7a0de768d95b95ad0b904f
Luciano Cordeiro <lacsousa@yahoo.com.br>
2019-05-05 21:42:56 -0300

    Monkey patch

--- aluraframe/client/js/app/services/ConnectionFactory.js
+++ aluraframe/client/js/app/services/ConnectionFactory.js
@@ -1,12 +1,15 @@
 var ConnectionFactory = (function () {
 // Essa conversão da Classe numa Função Anônima e retornando variáveis 
 // e um Classe é um Padrão de Projeto chamado Module
+// A variável ConnectionFactory tem um escopo global, mas o restante não
 
-    var stores = ['negociacoes'];
-    var version = 4;
-    var dbName = 'aluraframe';
+    const stores = ['negociacoes'];
+    const version = 4;
+    const dbName = 'aluraframe';
 
     var connection = null;
+
+    var close = null; 
 
     return class ConnectionFactory {
 
@@ -25,9 +28,16 @@ var ConnectionFactory = (function () {
                 };
 
                 openRequest.onsuccess = e => {
-
                     //0, "", null, undefined .. tudo isso é Falso
-                    if (!connection) connection = e.target.result;
+                    if (!connection) {
+                        connection = e.target.result;
+
+                        // Monkey Patch
+                        close = connection.close.bind(connection);
+                        connection.close = function(){
+                            throw new Error('Você não pode fechar diretmente a conexão');
+                        };
+                    }
                     resolve(connection);
                 };
 
@@ -40,15 +50,22 @@ var ConnectionFactory = (function () {
 
         static _createStores(connection) {
             //criando novas stores
-            stores.forEach(strore => {
-                if (e.target.result.objectStoreNames.contains(store))
-                    e.target.result.deleteObjectStore(store);
+            stores.forEach(store => {
+                if (connection.objectStoreNames.contains(store))
+                    connection.deleteObjectStore(store);
 
-                e.target.result.createObjectStore(store, {
+                connection.createObjectStore(store, {
                     autoIncrement: true
                 });
             });
 
         }
+
+        static closeConnection(){
+            if(connection){
+                close();
+                connection = null;
+            }
+        }
     }
-})();
+}) (); // Função AutoInvocada
