ae21aad81ce5add50f75c935fe3f6c6297d99d68
Luciano Cordeiro <lacs@petrobras.com.br>
2019-05-08 16:42:34 -0300

    Iniciando o pattern DAO

---
+++ aluraframe/client/js/app/dao/NegociacaoDao.js
@@ -0,0 +1,27 @@
+class NegociacaoDao {
+
+    constructor(connection) {
+        this._connection = connection;
+        this._store = 'negociacoes';
+    }
+
+    adiciona(negociacao) {
+        return new Promise((resolve, reject) => {
+
+            let request = this._connection
+                .transaction([this._store], 'readwrite')
+                .objectStore(this._store)
+                .add(negociacao);
+
+            request.onsuccess = e => {
+                console.log('Negociação incluída com sucesso');
+                resolve();
+            };
+
+            request.onerror = e => {
+                console.log(e.target.error);
+                reject('Não foi possível incluir a negociação');
+            };
+        });
+    }
+}
